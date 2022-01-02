032baa04d82ebe81ae7f3e1e9d8432151647210d
rksgr <r_kjha@yahoo.com>
2022-01-02 19:16:30 +0530

    Refactored: Invalid Mood case inform user

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,9 +1,12 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class MoodAnalyser {
     private static String message;
+    enum UserMood {
+        NULL,
+        EMPTY
+    }
 
     // Default constructor (no arguments)
     public MoodAnalyser(){}
@@ -47,9 +50,9 @@ public class MoodAnalyser {
         return mood;
     }
     /*
-    Use Case 2: Handle exception if user provides invalid mood
+    Use Case 2: Handle Exception if entered invalid mood
      */
-    public static String analyseMoodHandleInvalid() {
+    public static String analyseMoodHandleInvalid(){
         String mood = "";
         try{
             Pattern pattern1 = Pattern.compile("[H][a][p][p][y]");
@@ -64,15 +67,44 @@ public class MoodAnalyser {
         }
         // Catch exception if a user provides invalid mood
         catch (NullPointerException e) {
-            mood =  "Happy";
+            mood = "Happy";
         }
         return mood;
     }
+    /*
+    Use Case 3: Inform user if entered invalid mood
+     */
+
+    public static String analyseMoodThrowCustomExcept() throws MoodAnalysisException{
+        String mood = "";
+        try{
+            Pattern pattern1 = Pattern.compile("[H][a][p][p][y]");
+            Pattern pattern2 = Pattern.compile("[S][a][d]");
+            Matcher matcher1 = pattern1.matcher(message);
+            Matcher matcher2 = pattern2.matcher(message);
+            if (matcher1.find()){
+                mood = "Happy";
+            } else if (matcher2.find()){
+                mood = "Sad";
+            }
+        }
+        // Catch exception if a user provides invalid mood
+        catch (NullPointerException e) {
+            System.out.println(message);
+            throw new MoodAnalysisException("Please enter an appropriate message.");
+        }
+        return mood;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to BridgeLabz!");
         System.out.println("Today we shall analyse mood of a person.");
-        MoodAnalyser moodanalyser = new MoodAnalyser("Feeling Sad");
-        String mood = moodanalyser.analyseMoodHandleInvalid();
-        System.out.println("The mood of the person is: "+ mood);
+        MoodAnalyser moodanalyser = new MoodAnalyser(null);
+        try {
+            String mood = moodanalyser.analyseMoodThrowCustomExcept();
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
+        UserMood um = UserMood.EMPTY;
     }
 }
---
+++ MoodAnalysisException.java
@@ -0,0 +1,9 @@
+public class MoodAnalysisException extends Exception{
+    public MoodAnalysisException(String message){
+        super(message);
+    }
+    enum UserMood{
+        NULL,
+        EMPTY
+    }
+}
