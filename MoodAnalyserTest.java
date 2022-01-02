dc3797e128b8d8175b64ab35461ee1dc29f0540d
rksgr <r_kjha@yahoo.com>
2022-01-02 13:11:00 +0530

    Added: Test Cases for Happy and Sad mood

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -1,6 +1,24 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class MoodAnalyserTest {
-    public static void main(String[] args) {
-        System.out.println("Welcome to BridgeLabz!");
-        System.out.println("Today we are going to analyse the mood.");
+    /*
+    Test Case 1.1: Sad mood message should return sad
+     */
+    @Test
+    public void givenMessage_WhenSad_shouldReturnSad(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a Sad Message");
+        Assert.assertEquals("Sad",mood);
     }
+    /*
+    Test Case 1.2: Happy mood message should return Happy
+     */
+    @Test
+    public void givenMessage_WhenNotSad_shouldReturnHappy(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a Happy Message");
+        Assert.assertEquals("Happy",mood);
+    }
+
 }
