59aa0f4a4f5ff1f82cbc0c357236910af5134919
rksgr <r_kjha@yahoo.com>
2022-01-02 13:02:23 +0530

    Added: Welcome Message

---
+++ MoodAnalyserTest.java
@@ -0,0 +1,6 @@
+public class MoodAnalyserTest {
+    public static void main(String[] args) {
+        System.out.println("Welcome to BridgeLabz!");
+        System.out.println("Today we are going to analyse the mood.");
+    }
+}
